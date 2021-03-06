290ea812d56a744d9de75bf94926b78a64e4c228
umang talpara <umangpatel607@gmail.com>
2021-03-06 23:57:28 +0530

    update client

--- public/client.js
+++ public/client.js
@@ -1,1 +1,25 @@
-const socket = io
+const socket = io()
+let name;
+let messageArea =document.querySelector('.message_area') 
+let textarea = Document.querySelector('#textarea')
+do
+{
+    name = prompt('plz enter your name:')
+}while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(msg){
+    let msg ={
+        user:name,
+        message:msg
+    }
+    //append
+    appendMessage(msg, 'outgoing')
+}
+
+function appendMessage()
